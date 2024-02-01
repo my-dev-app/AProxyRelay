3e5b28b75b0d10976fa06bd06b22e113103b7f83
undeƒined <my.development.business@gmail.com>
2024-02-01 18:48:32 +0100

    ADD: New proxy website

---
+++ aproxyrelay/scrapers/parser_proxyscrape_http.py
@@ -0,0 +1,47 @@
+# -*- mode: python ; coding: utf-8 -*-
+"""
+░░      ░░       ░░       ░░░      ░░  ░░░░  ░  ░░░░  ░       ░░        ░  ░░░░░░░░      ░░  ░░░░  ░
+▒  ▒▒▒▒  ▒  ▒▒▒▒  ▒  ▒▒▒▒  ▒  ▒▒▒▒  ▒▒  ▒▒  ▒▒▒  ▒▒  ▒▒  ▒▒▒▒  ▒  ▒▒▒▒▒▒▒  ▒▒▒▒▒▒▒  ▒▒▒▒  ▒▒  ▒▒  ▒▒
+▓  ▓▓▓▓  ▓       ▓▓       ▓▓  ▓▓▓▓  ▓▓▓    ▓▓▓▓▓    ▓▓▓       ▓▓      ▓▓▓  ▓▓▓▓▓▓▓  ▓▓▓▓  ▓▓▓    ▓▓▓
+█        █  ███████  ███  ██  ████  ██  ██  █████  ████  ███  ██  ███████  ███████        ████  ████
+█  ████  █  ███████  ████  ██      ██  ████  ████  ████  ████  █        █        █  ████  ████  ████
+By undeƒined
+------------
+
+Main parser example, other parsers can inherit from this class
+"""
+from queue import Queue
+
+from .parser import MainScraper
+
+
+class ParserProxyScrapeHttp(MainScraper):
+    def __init__(self) -> None:
+        MainScraper.__init__(self)
+        self.zone = None
+
+    @classmethod
+    async def format_url(cls, url, zone: str = 'us', *args, **kwargs) -> str:
+        """Formats URL before scraping, let us adjust query parameters for each parser"""
+        cls.zone = zone
+        return url.replace('country=NL', f'country={zone.upper()}')
+
+    @classmethod
+    async def format_raw(cls, html: str) -> list:
+        """Parse text/html pages, customized method for the parser of this website"""
+        return [
+            {
+                'zone': cls.zone.upper(),
+                'method': 'http',
+                'anonymity': 'unknown',
+                'protocol': 'http',
+                'port': item.split(':')[1],
+                'ip': item.split(':')[0],
+            } for item in html.split('\r\n') if item
+        ]
+
+    @classmethod
+    async def format_data(cls, zone: str, data: dict, queue: Queue) -> None:
+        """Data formatter, formats data and returns is back in the process Queue"""
+        queue.put(data)
+        return queue
