8815adccf769ff745621a46e67c067c4ccb3d195
0x78f1935 <my.development.business@gmail.com>
2024-09-03 16:28:36 +0200

    ADD: New proxy sources (5)

---
+++ aproxyrelay/scrapers/parser_proxyscraper_com.py
@@ -0,0 +1,53 @@
+# -*- mode: python ; coding: utf-8 -*-
+"""
+░░      ░░       ░░       ░░░      ░░  ░░░░  ░  ░░░░  ░       ░░        ░  ░░░░░░░░      ░░  ░░░░  ░
+▒  ▒▒▒▒  ▒  ▒▒▒▒  ▒  ▒▒▒▒  ▒  ▒▒▒▒  ▒▒  ▒▒  ▒▒▒  ▒▒  ▒▒  ▒▒▒▒  ▒  ▒▒▒▒▒▒▒  ▒▒▒▒▒▒▒  ▒▒▒▒  ▒▒  ▒▒  ▒▒
+▓  ▓▓▓▓  ▓       ▓▓       ▓▓  ▓▓▓▓  ▓▓▓    ▓▓▓▓▓    ▓▓▓       ▓▓      ▓▓▓  ▓▓▓▓▓▓▓  ▓▓▓▓  ▓▓▓    ▓▓▓
+█        █  ███████  ███  ██  ████  ██  ██  █████  ████  ███  ██  ███████  ███████        ████  ████
+█  ████  █  ███████  ████  ██      ██  ████  ████  ████  ████  █        █        █  ████  ████  ████
+By undeƒined
+------------
+
+Main parser example, other parsers can inherit from this class
+"""
+from queue import Queue
+
+from .parser import MainScraper
+
+
+class ParserScraperComProxy(MainScraper):
+    def __init__(self) -> None:
+        MainScraper.__init__(self)
+        self.zone = None
+
+    @classmethod
+    async def format_url(cls, url, *args, **kwargs) -> str:
+        """Formats URL before scraping, let us adjust query parameters for each parser"""
+        cls.zone = kwargs.get("country", "us")
+        return url
+
+    @classmethod
+    async def format_raw(cls, html: str) -> list:
+        """Parse text/html pages, customized method for the parser of this website"""
+        result = []
+        data = html.split('\r\n')
+        for proxy in data:
+            if proxy:
+                protocol = proxy.split('://')[0]
+                ip = proxy.split('://')[1].split(':')[0]
+                port = proxy.split('://')[1].split(':')[-1]
+                result.append({
+                    'zone': cls.zone.upper(),
+                    'method': protocol,
+                    'anonymity': 'unknown',
+                    'protocol': protocol,
+                    'port': port,
+                    'ip': ip
+                })
+        return result
+
+    @classmethod
+    async def format_data(cls, zone: str, data: dict, queue: Queue) -> None:
+        """Data formatter, formats data and returns is back in the process Queue"""
+        queue.put(data)
+        return queue
